efbae0c6c06f3b7f16599df995bea724bd77a051
aljb <aljb@itu.dk>
2021-10-31 14:26:14 +0000

    Revert "Remove all references to UID from client"
    This reverts commit b865ead8bf946a76072093e1a706486d607dfe2c.

--- client/main.go
+++ client/main.go
@@ -15,7 +15,6 @@ import (
 
 // This is dirty
 var (
-	uid        string
 	username   string
 	clock      service.VectorClock
 	clockMutex sync.Mutex
@@ -42,42 +41,33 @@ func main() {
 	}
 
 	msg, err := stream.Recv()
-	if msg.Event != service.UserMessage_SET_UID || err != nil {
-		log.Fatalf("Failed to get uid: %v", err)
-	}
-
-	uid = msg.User
-	clock = msg.Message.Clock
-
-	clockMutex.Lock()
-	clock[uid]++
-	clockMutex.Unlock()
-
-	msg, err = stream.Recv()
 	if msg.Event != service.UserMessage_SET_USERNAME || err != nil {
-		log.Fatalf("Failed to get usernamerequest: %v", err)
+		log.Fatalf("Failed to get username request: %v", err)
 	}
 
-	clockMutex.Lock()
-	clock[uid]++
-	fmtClock := service.FormatVectorClockAsString(clock)
-	clockMutex.Unlock()
+	var insertedUsername string
+	fmt.Printf("Please enter wanted username: ")
+	fmt.Scan(&insertedUsername)
 
-	fmt.Printf("%v\nPlease enter wanted username: ", fmtClock)
-	fmt.Scan(&username)
-
-	message := service.Message{Clock: clock, Content: username}
+	clock = msg.Message.Clock
+	message := service.Message{Clock: clock, Content: insertedUsername}
 	stream.Send(&message)
+
+	username = msg.User
+
+	clockMutex.Lock()
+	clock[username]++
+	clockMutex.Unlock()
 
 	go listenForMessages(stream)
 
 	for i := 0; i < 10; i++ {
 		clockMutex.Lock()
-		clock[uid]++
+		clock[username]++
 		clockMutex.Unlock()
 		message := service.Message{Clock: clock, Content: strconv.Itoa(i)}
 		stream.Send(&message)
-		log.Printf("%v You (%s): %s\n", service.FormatVectorClockAsString(message.Clock), uid, message.Content)
+		log.Printf("%v You (%s): %s\n", service.FormatVectorClockAsString(message.Clock), username, message.Content)
 		time.Sleep(1000 * time.Millisecond)
 	}
 
@@ -94,14 +84,11 @@ func listenForMessages(stream service.Chittychat_ChatSessionClient) {
 
 		clockMutex.Lock()
 		clock = service.MergeClocks(clock, msg.Message.Clock)
-		clock[uid]++
+		clock[username]++
 		fmtClock := service.FormatVectorClockAsString(clock)
 		clockMutex.Unlock()
 
 		switch msg.Event {
-		case service.UserMessage_SET_UID:
-			log.Printf("%v set uid to %s\n", fmtClock, msg.User)
-			uid = msg.User
 		case service.UserMessage_INVALID_USERNAME:
 			log.Printf("%v Username %s already taken. Please rejoin with new name\n", fmtClock, username)
 			os.Exit(1)
