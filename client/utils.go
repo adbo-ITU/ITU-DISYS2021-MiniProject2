aa322b36788c11dec08e55d041c3f2f19bd89605
Adrian Borup <avborup@gmail.com>
2021-11-02 19:42:52 +0100

    Fix Ctrl+Backspace making weird output

--- client/utils.go
+++ client/utils.go
@@ -5,6 +5,6 @@ import "regexp"
 // A simple regex to distinguish UI event strings from typed letters.
 // I.e. "<Enter>" is illegal, and "y" is legal
 func IsLegalCharacter(c string) bool {
-	matched, err := regexp.Match(`^<[\w-]+>`, []byte(c))
+	matched, err := regexp.Match(`^<.+>`, []byte(c))
 	return err == nil && !matched
 }
